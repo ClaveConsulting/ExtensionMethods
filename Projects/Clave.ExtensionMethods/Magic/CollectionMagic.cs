848cef68086b670d38e7d6ccdabab69388569bb4
Marius Gundersen <me@mariusgundersen.net>
2019-07-29 12:17:59 +0200

    Support non-generic Collections
    ICollection does not have an Add method, only IList has it

--- Projects/Clave.ExtensionMethods/Magic/CollectionMagic.cs
+++ Projects/Clave.ExtensionMethods/Magic/CollectionMagic.cs
@@ -1,10 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace Clave.ExtensionMethods.Magic
 {
     public static class CollectionMagic
     {
         public static void Add<T>(this ICollection<T> list, IEnumerable<T> items)
+        {
+            if(items == null)
+                return;
+
+            foreach(var item in items)
+            {
+                list.Add(item);
+            }
+        }
+        
+        public static void Add(this IList list, IEnumerable items)
         {
             if(items == null)
                 return;
