c89f821b000366e9c03ea07f9c0dfda24370f9b5
Marius Gundersen <me@mariusgundersen.net>
2019-07-29 12:01:41 +0200

    Collection magic should work on collections, not just lists

--- Projects/Clave.ExtensionMethods/Magic/CollectionMagic.cs
+++ Projects/Clave.ExtensionMethods/Magic/CollectionMagic.cs
@@ -4,8 +4,13 @@ namespace Clave.ExtensionMethods.Magic
 {
     public static class CollectionMagic
     {
-        public static void Add<T>(this List<T> list, IEnumerable<T> items)
-            => list.AddRange(items);
+        public static void Add<T>(this ICollection<T> list, IEnumerable<T> items)
+        {
+            foreach(var item in items)
+            {
+                list.Add(item);
+            }
+        }
 
         public static void Add<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, IEnumerable<KeyValuePair<TKey, TValue>> items)
         {
